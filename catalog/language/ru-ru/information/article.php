a1a34886a35bcb548f4db8f1cb372e2cd8e7d344
TestMatick <iryna@testmatick.com>
2016-10-18 10:23:57 +0300

    update

---
+++ catalog/language/ru-ru/information/article.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_articles']        = 'Статьи';
+$_['text_subcategories']       = 'Подразделы:';
+$_['text_articles_list']       = 'Статьи раздела:';
+$_['text_error']        = 'Статья не найдена!';
+$_['text_readmore']        = 'читать далее';
+$_['text_tax']               = 'Без НДС:';
+$_['text_related']            = 'Связанные товары';
+$_['button_continue']       = 'перейти к разделам';
---
+++ catalog/language/ru-ru/module/latest_articles.php
@@ -0,0 +1,4 @@
+<?php
+
+$_['text_all_articles'] = 'смотреть всё';
+$_['button_readmore'] = 'читать далее';
